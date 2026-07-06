573d00fd2548d4de017b3458bd55bc14427b2931
swaroopchekuri09 <swaroopchekuri09@gmail.com>
2026-07-06 08:09:11 +0530

    Add files via upload

---
+++ maxsubarray1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class maxsubarray1{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int ans=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=i;j<n;j++){
+                sum+=arr[j];
+                ans=Math.max(ans,sum);
+            }
+        }
+        System.out.println(ans);
+    }
+}
---
+++ maxsubarray2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class maxsubarray2{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){   
+            arr[i]=sc.nextInt();
+        }
+        int ans=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int sum=0;
+                for(int k=i;k<=j;k++){
+                    sum+=arr[k];
+                }
+                ans=Math.max(ans,sum);
+            }
+        }
+        System.out.println(ans);
+    }
+}
