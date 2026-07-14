c88e83f93a285b7c7a7de2f35be7974532e2fb9d
swaroopchekuri09 <swaroopchekuri09@gmail.com>
2026-07-14 11:10:01 +0530

    Rename class from maxsubarray2 to MaxOfSubArray2

--- MaxOfSubArray2.java
+++ MaxOfSubArray2.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class maxsubarray2{
+public class MaxOfSubArray2{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
